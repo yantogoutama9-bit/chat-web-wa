3c1a1583118a1e6451aa48e8cb8007991c59147e
yantogoutama9-bit <yantogoutama9@gmail.com>
2026-02-05 14:47:57 +0700

    Update customer.js

--- js/customer.js
+++ js/customer.js
@@ -1,11 +1,17 @@
-let chat = JSON.parse(localStorage.getItem("chatCustomer")) || [];
+let chatCustomer = JSON.parse(localStorage.getItem("chatCustomer")) || [];
+let chatAdmin = JSON.parse(localStorage.getItem("chatAdmin")) || [];
 
 function renderChat() {
   const chatBox = document.getElementById("chat-box");
   chatBox.innerHTML = "";
-  chat.forEach(msg => {
+
+  // Gabung pesan Customer + Admin
+  let combined = [...chatCustomer, ...chatAdmin];
+  combined.forEach(msg => {
     chatBox.innerHTML += `<p><b>${msg.name} (${msg.phone}):</b> ${msg.text}</p>`;
   });
+
+  chatBox.scrollTop = chatBox.scrollHeight;
 }
 
 function sendMessage() {
@@ -15,8 +21,8 @@ function sendMessage() {
 
   if(!name || !phone || !input) return alert("Nama, No. Telepon, dan Pesan harus diisi!");
 
-  chat.push({ name, phone, text: input, sender: "Customer" });
-  localStorage.setItem("chatCustomer", JSON.stringify(chat));
+  chatCustomer.push({ name, phone, text: input });
+  localStorage.setItem("chatCustomer", JSON.stringify(chatCustomer));
 
   document.getElementById("msg-input").value = "";
   renderChat();
