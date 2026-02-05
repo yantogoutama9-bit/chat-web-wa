db93a1353506e946fb732146b02ab6fdff077c96
yantogoutama9-bit <yantogoutama9@gmail.com>
2026-02-05 14:21:56 +0700

    Delete public directory

--- public/sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', e => console.log('SW Installed'));
-self.addEventListener('activate', e => console.log('SW Activated'));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
