392e7d2d35c69ce1f85bfdd9eee1c35f813e3566
yantogoutama9-bit <yantogoutama9@gmail.com>
2026-02-05 13:08:12 +0700

    Add files via upload

---
+++ public/admin.js
@@ -0,0 +1,20 @@
+let chat = JSON.parse(localStorage.getItem("chatCustomer")) || [];
+
+function renderChat() {
+  const chatBox = document.getElementById("chat-box");
+  chatBox.innerHTML = "";
+  chat.forEach(msg => {
+    chatBox.innerHTML += `<p><b>${msg.sender}:</b> ${msg.text}</p>`;
+  });
+}
+
+function sendMessage() {
+  const input = document.getElementById("msg-input");
+  if(input.value.trim() === "") return;
+  chat.push({ sender: "Admin", text: input.value });
+  localStorage.setItem("chatCustomer", JSON.stringify(chat));
+  input.value = "";
+  renderChat();
+}
+
+renderChat();
